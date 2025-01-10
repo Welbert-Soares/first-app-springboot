8b99ce2315f7ed8d46da6f9aa7ca7e9c7027b50a
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-01-10 18:01:32 -0300

    Initial commit

---
+++ src/main/java/com/w3soft/firstappspringboot/FirstAppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.w3soft.firstappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstAppSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/w3soft/firstappspringboot/FirstAppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w3soft.firstappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
